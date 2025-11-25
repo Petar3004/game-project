912627c9fbdb7a07c1c85e3ff8ec6fe8a1fb0471
Petar3004 <pgrigorov@unibz.it>
2025-11-25 22:44:24 +0700

    refactored gear movement, locking doors, 1.2.1 and 1.2.2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    private Vector3[] cameraPositions;
+    private Dictionary<int, Vector3[]> cameraPositions;
     public int currentRoomIndex = 0;
     public int numberOfRooms;
     public static CameraController instance;
@@ -22,32 +23,34 @@ public class CameraController : MonoBehaviour
             Destroy(gameObject);
         }
 
-        if (cameraPositions == null || cameraPositions.Length == 0)
+        if (cameraPositions == null || cameraPositions.Count == 0)
         {
-            cameraPositions = new Vector3[]
+            cameraPositions = new Dictionary<int, Vector3[]>
             {
-            new Vector3(-17.86f, 0, -10f),
-            new Vector3(0, 0, -10f),
-            new Vector3(17.77f, 0, -10f)
+                { 0, new Vector3[] { new Vector3(-17.86f, 0, -10f), new Vector3(0, 0, -10f), new Vector3(17.77f, 0, -10f) } },
+                { 1, new Vector3[] { new Vector3(-17.86f, 0, -10f), new Vector3(-17.86f, 10, -10f), new Vector3(-17.86f, 20, -10f) } }
             };
         }
     }
 
     public void MoveCameraToNextRoom()
     {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
         currentRoomIndex += 1;
-        Camera.main.transform.position = cameraPositions[currentRoomIndex];
+        Camera.main.transform.position = cameraPositions[currentLevelIndex][currentRoomIndex];
     }
 
     public void MoveCameraToPreviousRoom()
     {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
         currentRoomIndex -= 1;
-        Camera.main.transform.position = cameraPositions[currentRoomIndex];
+        Camera.main.transform.position = cameraPositions[currentLevelIndex][currentRoomIndex];
     }
 
     public void MoveCameraToRoom(int roomIndex)
     {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
         currentRoomIndex = roomIndex;
-        Camera.main.transform.position = cameraPositions[roomIndex];
+        Camera.main.transform.position = cameraPositions[currentLevelIndex][roomIndex];
     }
 }
