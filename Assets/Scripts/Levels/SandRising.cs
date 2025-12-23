6d8046b512303fb174d843e43dda9007c6ceba00
Amme <arminplattner00@gmail.com>
2025-12-23 20:42:21 +0700

    chapter 2, level 2 updated

---
+++ Assets/Scripts/Levels/SandRising.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SandRising : MonoBehaviour
+{
+    public float riseSpeed = 0.5f;   // units per second
+    public float maxHeight = 10f;    // room height
+    private Vector3 startScale;
+
+    void Start()
+    {
+        startScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        float newHeight = transform.localScale.y + riseSpeed * Time.deltaTime;
+
+        if (newHeight <= maxHeight)
+        {
+            transform.localScale = new Vector3(
+                startScale.x,
+                newHeight,
+                startScale.z
+            );
+        }
+
+        GetComponent<SpriteRenderer>().material.mainTextureOffset += new Vector2(0, Time.deltaTime * 0.1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerObject"))
+        {
+            ManagersRoot.instance.gameManager.RestartLevel();
+        }
+    }
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour
         { 2, new Vector3[] { new Vector3(-11.5f, -4.1f, 0f), new Vector3(-24.68f, 5.78f, 0), new Vector3(-11.53f, 15.84f, 0) } },
         { 3, new Vector3[] { new Vector3(0.12f, -3.7f, 0) } },
         { 4, new Vector3[] { new Vector3(-7.8f, -3.75f, 0), Vector3.zero, Vector3.zero } },
-        { 5, new Vector3[] { new Vector3(-25.03f, -0.24f, 0), new Vector3(23.92f, -0.33f, 0), Vector3.zero } },
+        { 5, new Vector3[] { new Vector3(-25.03f, -0.24f, 0), new Vector3(47.62f, -0.36f, 0), Vector3.zero } },
         { 6, new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero } },
         { 7, new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero } },
         { 8, new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero } },
